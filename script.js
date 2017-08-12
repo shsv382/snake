4c9d0ac5b4488d7c8a912776b7c503e4e2deb494
shsv382 <shsv382@gmail.com>
2017-08-12 12:18:08 +0400

    Clean code

--- script.js
+++ script.js
@@ -1,87 +1,80 @@
 function fullFielf(size){
-	var fld = [];
-	for (var i=0; i<30; i++) {
-		fld[i] = [];
-		for (var j = 0; j<40; j++) {
-			fld[i][j] = {
-				content: " ",	//Math.floor(Math.random()*9),
-				top: i,
-				left: j
-			};
-			addDiv(fld[i][j], size);
-		}
+	var field = document.getElementsByClassName("field")[0];
+	for (var i = 0; i < 1200; i++) {
+		var cube = document.createElement('div');
+		cube.classList.add("cube");
+		cube.index = i;
+		field.appendChild(cube);
 	}
-	return fld;
-}
-
-function addDiv(fld, size){
-	$("div.field").append('<div class="cube" id="div-' + fld.left + '-' + fld.top + '"></div>');
-	/*$("div#div-" + fld.left + "-" + fld.top).css('width', '2.5%');
-	$("div#div-" + fld.left + "-" + fld.top).css('height', '2.5%');
-	//$("div#div-" + fld.left + "-" + fld.top).css('top', fld.top*size/80);	
-	//$("div#div-" + fld.left + "-" + fld.top).css('left', fld.left*size/80);
-	$("div#div-" + fld.left + "-" + fld.top).css('padding', '0');
-	$("div#div-" + fld.left + "-" + fld.top).css('margin', '0');*/
 }
 
 
 function Snake() {
-	this.body = [[20,20],[19,20],[18,20]];
+	this.body = [599,598,597];
 	this.speed = 3;
 	this.direction = "right";
   	this.timerId = null;
 	this.food = undefined;
 
 	var checkDirection = function() {
+		
+		var next;
+		var cubes = document.getElementsByClassName("cube");
+
 		switch(this.direction) {
 	  	    case 'right':
-	    		this.body[0][0]++;
+	    		next = this.body[0] + 1;
 	    		break;
 	    	case 'left':
-	    		this.body[0][0]--;
+	    		next = this.body[0] - 1;
 	    		break;
 	    	case 'bottom':
-	    		this.body[0][1]++;
+	    		next = this.body[0] + 40;
 	    		break;
 	    	case 'top':
-	    		this.body[0][1]--;
+	    		next = this.body[0] - 40;
 	    		break;
 		}
-		for (var i = this.body.length - 1; i > 0; i--) {
-  			this.body[i] = this.body[i-1];		
-  		}	
-	}.bind(this);
 
-	var renewal = function() {
-		if (this.body[0][0] == 40) this.body[0][0] = 0;
-		if (this.body[0][1] == 30) this.body[0][1] = 0;
-		if (this.body[0][0] == -1) this.body[0][0] = 39;
-		if (this.body[0][1] == -1) this.body[0][1] = 29;
-	}.bind(this);
+		if ((next) % 40 == 0 && this.direction == "right") {
+			next -= 40;
+		}
+		if ((next + 1) % 40 == 0 && this.direction == "left") {
+			next += 40;
+		}
+		if (next < 0) next += 1200;
+		if (next > 1200) next -= 1200;
 
-	var stepResults = function() {
-		console.log(this.body.length);
-		console.log("Speed: " + this.speed);
-	}.bind(this)
+		if (this.body.indexOf(next) != -1) {
+			var field = document.getElementsByClassName("field")[0];
+			field.innerHTML = "Game Over!";
+			field.classList.add("gameover");
+		}
 
-	var run = function() {		
-		checkDirection();
-		renewal();
-		stepResults();		
-		makeFood();	
-		eat();
-		this.body.forEach(function(item, i, arr) {			
-    		$("#div-" + item[0] + "-" + item[1]).addClass("snaked");
-  		});
-  		var arr = this.body;
+		this.body.unshift(next);
+		cubes[this.body[0]].classList.add("snaked");
+		if (this.body[0] != this.food) {
+			cubes[this.body.pop()].classList.remove("snaked");
+		}
+		else {
+			cubes[this.food].classList.remove("food");;
+		}
   		
-  		this.speed = 2 + Math.floor(this.body.length / 3);
+  		
+  	}.bind(this);
+
+	var run = function() {	
+  		checkDirection();
+		makeFood();	
+  		var j = this.body.length - 1;
+  		this.speed = 2 + Math.floor(j / 3);
+  		this.timerId = setTimeout(this.go, 400/this.speed);
 	}.bind(this);
 
   
   	this.go = function() {
     	run();
-    	this.timerId = setInterval(this.go, 1000/this.speed);
+    	//this.timerId = setTimeout(this.go, 100/this.speed);
   	};
   
   	this.stop = function() {
@@ -89,24 +82,14 @@ function Snake() {
   	};
 
   	var makeFood = function() {
-  		if (!this.food) {
-	  		this.food = new Array(2);
-	  		this.food[0] = (Math.floor(Math.random() * 40));
-	  		this.food[1] = (Math.floor(Math.random() * 30));
-	  		$("#div-" + this.food[0] + "-" + this.food[1]).addClass("food");
-	  	}
+  		if (!this.food || this.body.indexOf(this.food) != -1) {
+  			this.food = (Math.floor(Math.random() * 1200));
+  		}
+		var cubes = document.getElementsByClassName("cube");
+		cubes[this.food].classList.add("food");
 	  	return false;
   	}.bind(this);
 
-  	var eat = function() {
-  		if (this.food[0] == this.body[0][0] &&
-  			this.food[1] == this.body[0][1]) {
-  			this.body.unshift(this.food);
-  			$("#div-" + this.food[0] + "-" + this.food[1]).removeClass("food");
-  			this.food = null;
-  		}
-  		return false;
-  	}.bind(this);
 }
 
 
@@ -121,6 +104,23 @@ $(document).ready(function(){
 		snake.stop();
 	});
 
+	document.onkeydown = function(e) {
+		switch (e.keyCode) {
+			case 37:
+				snake.direction = "left";
+				break;
+			case 38:
+				snake.direction = "top";
+				break;
+			case 39:
+				snake.direction = "right";
+				break;
+			case 40:
+				snake.direction = "bottom";
+				break;
+		}
+	}
+
 	$("#up").on('click', function() {
 		snake.direction = "top";
 	});
@@ -133,5 +133,6 @@ $(document).ready(function(){
 	$("#right").on('click', function() {
 		snake.direction = "right";
 	});
+
 
 });
